54ab8e709e1eb0cb9af9b5ea9a3eed236ac622f5
D0471750 <playwithme401@gmail.com>
2020-02-18 12:25:55 +0800

    Modify Binary_Tree.py 2020/2/18

--- C_To_Python/Tree_Python_Folder/Binary_Tree.py
+++ C_To_Python/Tree_Python_Folder/Binary_Tree.py
@@ -9,46 +9,57 @@ class Node():
     #==============================
     #建構子
     #==============================
-    def __init__(self,Value):
+    def __init__(self,Value,ID):
         #值
         self.Value = Value
         #左邊子節點
         self.Left = None
         #右邊子節點
         self.Right = None
+        #編號
+        self.ID = ID
 
-
-    #==============================
-    #方法: 新增左子節點(根節點，新增的Node)
-    #==============================
-    def add_binary_tree_left_Node(self, New_Value):
-        #如果根節點沒有左子節點
-        if(self.Left == None):
-            self.Left = Node(New_Value)
+    def add_binary_tree_Node(self, New_Value, ID):
+        #如果ID偶數，則表示此Node是在左邊
+        if (ID%2 == 0):
+            #如果根節點沒有左子節點
+            if (self.Left == None):
+                self.Left = Node(New_Value, ID)
+            #如果根節點有左子節點
+            else:
+                self.Left.add_binary_tree_Node(New_Value,ID)
+        #如果ID奇數，則表示此Node是在右邊
         else:
-            self.Left.add_binary_tree_left_Node(New_Value)
+            #如果根節點沒有右子節點
+            if (self.Right == None):
+                self.Right = Node(New_Value, ID)
+            #如果根節點有右子節點
+            else:
+                self.Right.add_binary_tree_Node(New_Value,ID)
 
     #==============================
-    #方法:新增右子節點(根節點，新增的Node)
+    #方法:印出Binary Tree(中序法)
     #==============================
-    def add_binary_tree_right_Node(self, New_Value):
-        #如果根節點沒有右子節點
-        if(self.Right == None):
-            self.Right = Node(New_Value)
-        else:
-            self.Right.add_binary_tree_right_Node(New_Value)
+    def print_binary_tree_inorder_traversal(self):
 
-
+        if (self.Left != None):
+            self.Left.print_binary_tree_inorder_traversal()
+        print(self.Value)
+        if (self.Right != None):
+            self.Right.print_binary_tree_inorder_traversal()
 #=================================================================================
 #執行
 #=================================================================================
 
 #List測試資料
-Nubmer_List = [1,2,3,4,5,6,7,8,9,10]
+Number_List = [2,3,4,5,6,7,8,9,10]
 
 #設置根節點
-Root = Node(Nubmer_List[0])
+Root = Node(Number_List[0] , 1)
 
-#建立二元樹
+#建立二元樹((i+1)是因為編號是從1開始，而List是從0開始)
+for i in range(1,len(Number_List)):
+    Root.add_binary_tree_Node(Number_List[i] , (i+1))
 
-
+#印出二元樹
+Root.print_binary_tree_inorder_traversal()
