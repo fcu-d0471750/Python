ba00f79ec66a3cd1610d13dc850b4aa491639c5c
D0471750 <playwithme401@gmail.com>
2020-03-17 12:42:59 +0800

    Add Decorator_1.py Descriptor_1.py 2020/3/17

--- Python_Learn/Class_Test/Decorator_1.py
+++ Python_Learn/Class_Test/Decorator_1.py
@@ -1,13 +1,13 @@
 '''
-裝飾器(Decorator、@):
+裝飾器(Decorator):
 主要用於多個方法的內部邏輯十分接近，所以透過裝飾器將共同的邏輯給拿出來做成裝飾器，而這些方法只要將不同的邏輯給寫在自身就可以了。
 盡量在整體程式有一定開發程度時再使用裝飾器。
 
-主要分成3個寫法無參數、有1個參數、有多個參數。
+主要分成3個寫法方法無參數、方法有1個參數、方法有多個參數、裝飾器有參數。
 '''
 
 '''
-# 無參數
+# 方法無參數
 def printHello(function):
     def wrapper():
         print("Hello")
@@ -22,21 +22,22 @@ printWorld()
 '''
 
 '''
-# 有1個參數
+# 方法有1個參數
 def printHello(function):
-    def wrapper(str):
+    def wrapper(arg):
         print("Hello")
-        return function(str)
+        return function(arg)
     return wrapper
 
 @printHello
-def printWorld(str):
-    print(str)
+def printWorld(arg):
+    print(arg)
 
 printWorld("Decorator")
 '''
 
-# 有多個參數
+'''
+# 方法有多個參數
 def printHello(function):
     def wrapper(*args , **kwargs):
         print("Hello")
@@ -56,3 +57,20 @@ def printmularg(str1 , number):
 printsinglearg("Decorator")
 print("---------")
 printmularg("Decorator" , 15)
+'''
+
+## 裝飾器也有參數
+def print_decorator_arg(decorator_arg):
+    def decorator(function):
+        def wrapper(*args, **kwargs):
+            print(decorator_arg)
+            return function(*args, **kwargs)
+        return wrapper
+    return decorator
+
+@print_decorator_arg("Hi")
+def sayHi_printArg(arg):
+    print(arg)
+
+
+sayHi_printArg("World")
--- Python_Learn/Class_Test/Descriptor_1.py
+++ Python_Learn/Class_Test/Descriptor_1.py
@@ -6,20 +6,52 @@
 
 
 
+# 描述器
+class Descriptor:
+
+    def __init__(self , name):
+        # 設定欄位名稱
+        self.name = name
+
+    def __get__(self, instance, owner):
+        # 檢查instance是否為None
+        if instance is None:
+            return self
+
+        return instance.__dict__[self.name]
+
+    def __set__(self, instance, value):
+        # 檢查value是否為int
+        if not isinstance(value , int):
+            raise TypeError(value , "is not int.")
+
+        # 找出instance中self.name的欄位，將值改為value
+        instance.__dict__[self.name] = value
+
+    def __delete__(self, instance):
+        instance.__dict__[self.name] = None
 
 
+# 使用描述器的類別
+class Clientclass:
+    number = Descriptor('number')
 
+    def __init__(self , value):
+        self.number = value
 
+# 執行
+# 實體化
+client = Clientclass(15)
 
+print("使用 __get__")
+print("client.number: " , client.number)
 
+print("使用 __set__")
+client.number = 55
+print("client.number: " , client.number)
 
-
-
-
-
-
-
-
-
+print("使用 __delete__")
+del client.number
+print("client.number: " , client.number)
 
 
